8bfe312d280f74ca4309c9285806fc2e500166c4
AngelX2000 <miguel_osorio11@hotmail.com>
2025-05-08 20:07:25 -0500

    peticiones

--- main.py
+++ main.py
@@ -1,28 +1,66 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,make_response
 
 app=Flask(__name__)
 
 if __name__=="main":
     app.run(debug=True)
 
-    @app.route('/buscar')
-    def buscar():
-     producto = request.args.get('producto')
-     talla = request.args.get('tamano')
-     color = request.args.get('color')
-     if producto is None and talla is None and color is None:
+@app.route('/buscar')
+def buscar():
+    producto = request.args.get('producto')
+    talla = request.args.get('tamano')
+    color = request.args.get('color')
+    if producto is None and talla is None and color is None:
         return "faltan datos"
-     return f"Buscando {producto} de talla {talla} y color {color}"
+    return f"Buscando {producto} de talla {talla} y color {color}"
     
+
+listado=[]
+
+
+
+
 @app.route("/registro", methods=["GET"])
 def ruta_formulario():
     return render_template("formulario.html")
 
-
+#este es el tipo de solicitud 2 que estamos enviando informacion por el body
 @app.route("/registro", methods=["POST"])
 def ruta_registro():
     nombre=request.form.get("estudiante")
     email=request.form.get("correo")
     password=request.form.get("contrasena")
-    return f"usuario registrado: {nombre}, {email}, {password}"
+    if nombre and email and password:
+        listado.append({"nombre":nombre,"correo":email,"password":password})
+        return render_template("formulario.html", listado=listado)
+
+    return f"faltan datos"
+
+
+#tipo de solicitud 3: parametros en la ruta
+
+@app.route("/ropa/<string:producto>/<string:talla>")
+def ruta_consulta(producto, talla):
+   return f"el producto consultado es {producto}, {talla}"
+
+
+#tipo de solicitud 4: headers http
+@app.route("/ver-headers")
+def ruta_headers():
+   navegador=request.headers.get("User-agent")
+   return f"el navegador que hace la peticion es {navegador}"
+
+
+#tipo de solicitud 5: manejo de cookies
+@app.route("/crear_cookies")
+def crear_cookie():
+   respuesta=make_response("cookie creada")
+   respuesta.set_cookie("usuario_logeado","true")
+   return respuesta
+
+@app.route("/leer_cookies")
+def leer_cookie():
+   valor=request.cookies.get("usuario_logeado")
+   return f"el valor de la cookie es: {valor}"
+
     
